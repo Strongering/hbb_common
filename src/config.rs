518e4604307ff861dd62038d468c1cd4e24af1ff
Strongering <86599999+Strongering@users.noreply.github.com>
2025-05-04 00:03:14 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["orangepi.jqstrong.asia"];
+pub const RS_PUB_KEY: &str = "X2f7Qtbccz4mAHEIsPfvgPyfS+12E0MRTw+9omqT14k=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
